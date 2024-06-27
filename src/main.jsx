099152bdf10669dddc847d8c716f24a55cbad8f5
Hafeez Hussain <hafeez.hussain2003@gmail.com>
2024-06-27 21:10:58 +0500

    firstapp

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+// 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello React Learner</h1>
+      <p>Welcome to the React Home...</p>
+      <img src="https://t4.ftcdn.net/jpg/00/69/19/61/360_F_69196153_GEET4JVdjXC1tfmBgx5Ge3dwx8Oyb1wi.jpg" alt="img" />
+    </div>
+  </React.StrictMode>
+);
+
